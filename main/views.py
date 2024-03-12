dfcef9f9115bfb499e17a38e013407c7a2349156
Nandika-A <nandikaagrawal610@gmail.com>
2024-03-12 12:12:08 +0530

    Fetch and display emotions

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def test_emotions(request):
+    request.session["emotion"] = "happy"
+    return HttpResponse("Emotion set to happy")
